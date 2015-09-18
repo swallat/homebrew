1edb42b8a0687430b38c008d5c33e91633ecffff
Martin Afanasjew <martin@afanasjew.de>
2015-09-18 14:13:38 +0200

    qt5: fix fatal build error with Xcode 7
    This was previously only enabled for OS X 10.11, but is an Xcode issue.

--- Library/Formula/qt5.rb
+++ Library/Formula/qt5.rb
@@ -30,14 +30,12 @@ class Qt5 < Formula
     # when this formula is updated to 5.5.1
     patch :DATA
 
-    # Upstream commit to fix the fatal build error on OS X El Capitan.
+    # Upstream commit to fix the fatal build error with Xcode 7.
     # https://codereview.qt-project.org/#/c/121545/
     # Should land in the 5.5.1 release.
-    if MacOS.version >= :el_capitan
-      patch do
-        url "https://raw.githubusercontent.com/DomT4/scripts/2107043e8/Homebrew_Resources/Qt5/qt5_el_capitan.diff"
-        sha256 "bd8fd054247ec730f60778e210d58cba613265e5df04ec93f4110421fb03b14a"
-      end
+    patch do
+      url "https://raw.githubusercontent.com/DomT4/scripts/2107043e8/Homebrew_Resources/Qt5/qt5_el_capitan.diff"
+      sha256 "bd8fd054247ec730f60778e210d58cba613265e5df04ec93f4110421fb03b14a"
     end
   end
 
